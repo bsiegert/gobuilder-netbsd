1ccd296c7f965759f7b986690a294939746251d4
Benny Siegert <bsiegert@gmail.com>
2023-12-29 18:28:04 +0100

    Add more installation steps
    - initialize daemontools at boot
    - copy certificates to the correct place
    - install the rc.d script

--- set-up-luci.sh
+++ set-up-luci.sh
@@ -17,12 +17,24 @@ make_dir() {
 	if [ -e $dir ]; then
 		echo "==> Directory $dir already exists." >&2
 	else
-		echo "==> Creating directory $1." >&2
+		echo "==> Creating directory $dir." >&2
 		install -d $* $dir
 	fi
 }
 
-
+copy_file() {
+	src=$1
+	dest=$2
+	srcfile=`basename $1`
+	if [ -e $dest/$srcfile ]; then
+		echo "==> file $dest/$srcfile already exists." >&2
+	elif [ ! -e $src ]; then
+		echo "==> missing file $src!" >&2
+	else
+		echo "==> copying $src to $dest." >&2
+		cp $src $dest
+	fi
+}
 
 install_go_bin() {
 	echo "==> Installing $1." >&2
@@ -46,13 +58,32 @@ make_dir /var/lib/luci_machine_tokend -o lucitoken
 if [ -e /service ]; then
 	echo "==> Directory /service already exists." >&2
 else
-	echo "==> Installing directory /service."
+	echo "==> Installing directory /service." >&2
 	pax -rw -pp service /
 fi
 
-if grep -q "Go builder settings" /etc/rc.conf; then
-	echo "==> /etc/rc.conf already contains Go builder settings."
+if grep -q svscanboot /etc/rc.local; then
+	echo "==> /etc/rc.local already contains daemontools initialization." >&2
 else
-	echo "==> Installing Go builder settings to /etc/rc.conf, please verify!"
+	echo "==> Starting daemontools at boot (/etc/rc.conf)." >&2
+	echo "d\nwq" | ed /etc/rc.local
+	cat rcd/rc.local >> /etc/rc.local
+	echo
+	tail /etc/rc.local
+fi
+
+copy_file rcd/newfs_scratch /etc/rc.d
+chmod 555 /etc/rc.d/newfs_scratch
+
+if grep -q "Go builder settings" /etc/rc.conf; then
+	echo "==> /etc/rc.conf already contains Go builder settings." >&2
+else
+	echo "==> Installing Go builder settings to /etc/rc.conf, please verify!" >&2
 	cat rcd/rc.conf >> /etc/rc.conf
 fi
+
+. ./rcd/rc.conf
+copy_file ${builder_name}.cert /home/lucitoken
+copy_file ${builder_name}.key /home/lucitoken
+chown lucitoken /home/lucitoken/${builder_name}.cert
+chown lucitoken /home/lucitoken/${builder_name}.key
